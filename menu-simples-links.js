4f205db93666d4df46928f5cf8af80da0766e6ef
ResultadosFacil <resultadosfacil@gmail.com>
2025-03-28 15:34:19 -0300

    Update menu-simples-links.js

--- menu-simples-links.js
+++ menu-simples-links.js
@@ -55,87 +55,87 @@
 
         container.innerHTML = `
             <div class="custom-menu-wrapper">
-                <div class="custom-menu-title">Resultados por Estado/Banca</div><hr style="height: 1px; border: none; background-color: #ccc;margin-bottom: 10px;">
+                <div class="custom-menu-title">Resultados por Estado/Banca</div>
 
                 <div class="custom-menu-container">
                     <div>
                         <p>Brasil</p>
                         <ul>
-                            <li><a href="/resultado-loteria-nacional-de-hoje.html">Loteria Nacional</a></li>
+                            <li><a href="/resultado-loteria-nacional-de-hoje">Loteria Nacional</a></li>
                         </ul>
                     </div>
                     <div>
                         <p>Bahia</p>
                         <ul>
-                            <li><a href="/resultado-maluca-bahia-de-hoje.html">Maluca Bahia</a></li>
-                            <li><a href="/resultado-paratodos-bahia-de-hoje.html">Paratodos Bahia</a></li>
+                            <li><a href="/resultado-maluca-bahia-de-hoje">Maluca Bahia</a></li>
+                            <li><a href="/resultado-paratodos-bahia-de-hoje">Paratodos Bahia</a></li>
                         </ul>
                     </div>
                     <div>
                         <p>Ceará</p>
                         <ul>
-                            <li><a href="/resultado-lotece-ceara-de-hoje.html">LOTECE - Loteria dos Sonhos</a></li>
+                            <li><a href="/resultado-lotece-ceara-de-hoje">LOTECE - Loteria dos Sonhos</a></li>
                         </ul>
                     </div>
                     <div>
                         <p>Goiás</p>
                         <ul>
-                            <li><a href="/resultado-look-goias-de-hoje.html">LOOK LOTERIAS</a></li>
+                            <li><a href="/resultado-look-goias-de-hoje">LOOK LOTERIAS</a></li>
                         </ul>
                     </div>
                     <div>
                         <p>Paraíba</p>
                         <ul>
-                            <li><a href="/resultado-lotep-pb-de-hoje.html">Lotep</a></li>
-                            <li><a href="/resultado-paratodos-pb-de-hoje.html">Paratodos PB</a></li>
+                            <li><a href="/resultado-lotep-pb-de-hoje">Lotep</a></li>
+                            <li><a href="/resultado-paratodos-pb-de-hoje">Paratodos PB</a></li>
                         </ul>
                     </div>
                     <div>
                         <p>Rio de Janeiro</p>
                         <ul>
-                            <li><a href="/resultado-pt-rio-de-hoje.html">PT-Rio</a></li>
+                            <li><a href="/resultado-pt-rio-de-hoje">PT-Rio</a></li>
                         </ul>
                     </div>
                     <div>
                         <p>São Paulo</p>
                         <ul>
-                            <li><a href="/resultado-pt-sp-de-hoje.html">PT-SP</a></li>
-                            <li><a href="/resultado-bandeirantes-sp-de-hoje.html">BANDEIRANTES</a></li>
+                            <li><a href="/resultado-pt-sp-de-hoje">PT-SP</a></li>
+                            <li><a href="/resultado-bandeirantes-sp-de-hoje">BANDEIRANTES</a></li>
                         </ul>
                     </div>
                     <div>
                         <p>Pernambuco</p>
                         <ul>
-                            <li><a href="/resultado-aval-pernambuco-de-hoje.html">AVAL Pernambuco</a></li>
-                            <li><a href="/resultado-caminho-da-sorte-pe-de-hoje.html">CAMINHO DA SORTE</a></li>
-                            <li><a href="/resultado-cooperativa-de-petrolina-de-hoje.html">Cooperativa de Petrolina-PE</a></li>
-                            <li><a href="/resultado-alianca-pe-de-hoje.html">EXTRAÇÃO ONLINE - ALIANÇA</a></li>
-                            <li><a href="/resultado-loteria-popular-de-hoje.html">LOTERIA POPULAR</a></li>
-                            <li><a href="/resultado-nordeste-monte-carlos-pe-de-hoje.html">NORDESTE MONTE CARLOS</a></li>
+                            <li><a href="/resultado-aval-pernambuco-de-hoje">AVAL Pernambuco</a></li>
+                            <li><a href="/resultado-caminho-da-sorte-pe-de-hoje">CAMINHO DA SORTE</a></li>
+                            <li><a href="/resultado-cooperativa-de-petrolina-de-hoje">Cooperativa de Petrolina-PE</a></li>
+                            <li><a href="/resultado-alianca-pe-de-hoje">EXTRAÇÃO ONLINE - ALIANÇA</a></li>
+                            <li><a href="/resultado-loteria-popular-de-hoje">LOTERIA POPULAR</a></li>
+                            <li><a href="/resultado-nordeste-monte-carlos-pe-de-hoje">NORDESTE MONTE CARLOS</a></li>
                         </ul>
                     </div>
                     <div>
                         <p>Brasília (DF)</p>
                         <ul>
-                            <li><a href="/resultado-lbr-brasilia-de-hoje.html">LBR</a></li>
+                            <li><a href="/resultado-lbr-brasilia-de-hoje">LBR</a></li>
                         </ul>
                     </div>
                     <div>
                         <p>Minas Gerais</p>
                         <ul>
-                            <li><a href="/resultado-minas-mg-de-hoje.html">MINAS-MG</a></li>
+                            <li><a href="/resultado-minas-mg-de-hoje">MINAS-MG</a></li>
                         </ul>
                     </div>
                     <div>
                         <p>Rio Grande do Sul</p>
                         <ul>
-                            <li><a href="/resultado-bicho-rs-de-hoje.html">Bicho RS</a></li>
+                            <li><a href="/resultado-bicho-rs-de-hoje">Bicho RS</a></li>
                         </ul>
                     </div>
                     <div>
                         <p>Sergipe</p>
                         <ul>
-                            <li><a href="/resultado-abaese-itabaiana-paratodos-de-hoje.html">ABAESE - ITABAIANA PARATODOS</a></li>
+                            <li><a href="/resultado-abaese-itabaiana-paratodos-de-hoje">ABAESE - ITABAIANA PARATODOS</a></li>
                         </ul>
                     </div>
                 </div>
