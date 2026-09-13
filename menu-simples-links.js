632d70b61a830d8001752e744ddba749c761dbd9
ResultadosFacil <resultadosfacil@gmail.com>
2026-09-13 17:25:46 -0300

    Update menu-simples-links.js

--- menu-simples-links.js
+++ menu-simples-links.js
@@ -90,7 +90,8 @@
             "https://bancafeiticeira.com.br/",
             "https://tikdasorte.net.br/",
             "https://sorteclub.net.br/",
-            "https://vaidarboa.com.br/"
+            "https://vaidarboa.com.br/",
+            "https://paratodosbrasil.info/registro?indicacao=LD078E2ZRB50"
         ];
 
         // Gera os ícones HTML extraindo automaticamente a favicon de cada domínio
